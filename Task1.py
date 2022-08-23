46e8d0b71b8748c1ad63ce66004a2c6510105793
AndriyPovzun <povzuun99@gmail.com>
2022-08-23 14:01:20 +0300

    Finish_First_Task

--- Task1.py
+++ Task1.py
@@ -1,3 +1,6 @@
-print("Hello Git!")
-
-print("Test new branch")
+employees_eleks = ['John Jones', 'Andy Ruis', 'Oleksandr Usyk',
+                   'Andrii Yevtushenko', 'Will Smith', 'Conor McGregor']
+employees_toshiba = ['Dana White', 'Kamaru Usman', 'George St-Pierre',
+                     'John Jones', 'Antony Joshua', 'Muhammad Ali']
+employees_toshiba.extend(employees_eleks)
+print(employees_toshiba)
